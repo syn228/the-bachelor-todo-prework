564b632fd975b5f4c68d766ba37ca5cc78437688
Aaron Na <syn228@nyu.edu>
2018-04-21 01:55:40 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,13 +1,38 @@
+require 'pry'
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  winner_firstname = nil
+  data.each do |season_number, stats|
+    if season_number == season
+      stats.each do |details|
+    if details["status"] == "Winner"
+      winner_firstname = details["name"].split(" ")[0]
+        end
+      end
+    end
+  end
+  winner_firstname
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season_number, stats|
+    stats.each do |details|
+      if details["occupation"] == occupation
+        return details["name"]
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  counter = []
+  data.each do |season_number, stats|
+    stats.each do |details|
+      if details["hometown"] = hometown
+        counter << season_number
+      end
+    end
+  end
 end
 
 def get_occupation(data, hometown)
