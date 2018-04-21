e8c2981f7faea63226fce336b189d6bcf2a76ae8
Aaron Na <syn228@nyu.edu>
2018-04-21 01:57:06 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -25,7 +25,7 @@ def get_contestant_name(data, occupation)
 end
 
 def count_contestants_by_hometown(data, hometown)
-
+ counter = 0
   data.each do |season_number, stats|
     stats.each do |details|
       if details["hometown"] = hometown
