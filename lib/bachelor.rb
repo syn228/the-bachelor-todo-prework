cd80f41e9b74006df50a8b30859ce643ea20ea38
Aaron Na <syn228@nyu.edu>
2018-04-21 02:08:05 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,57 @@
+require 'pry'
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  winner_firstname = nil
+  data.each do |season_number, stats|
+    if season_number == season
+      stats.each do |details|
+    if details["status"] == "Winner"
+      winner_firstname = details["name"].split(" ")[0]
+        end
+      end
+    end
+  end
+  winner_firstname
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season_number, stats|
+    stats.each do |details|
+      if details["occupation"] == occupation
+        return details["name"]
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+ counter = 0
+  data.each do |season_number, stats|
+    stats.each do |details|
+      if details["hometown"] == hometown
+        counter += 1
+      end
+    end
+  end
+  counter
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season_number, stats|
+    stats.each do |details|
+      if details["hometown"] == hometown
+        return details["occupation"]
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  age = data[season].collect do |details|
+    details["age"].to_i
+  end
+  total_age = age.reduce(0){|sum, the_age| sum += the_age}
+  total_contestants = age.count.to_f
+  (total_age/total_contestants).round
+      
 end
