fcafa46c9421f20b28ec865c8aafe1e78fe30c0f
Aaron Na <syn228@nyu.edu>
2018-04-21 02:06:04 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -51,6 +51,7 @@ def get_average_age_for_season(data, season)
     details["age"].to_i
   end
   total_age = age.reduce(0){|sum, the_age| sum += the_age}
-  total_contestants = 
+  total_contestants = age.count.to_f
+  
       
 end
